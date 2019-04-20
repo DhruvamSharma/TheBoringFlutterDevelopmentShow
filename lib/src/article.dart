45637628f1d89941c6568b5663bba3a74677c272
Dhruvam <dhruvamssharma@gmail.com>
2019-04-20 16:12:08 +0530

    completed ep1

---
+++ lib/src/article.dart
@@ -0,0 +1,90 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final int score;
+  final String age;
+  final int commentCount;
+
+  const Article(
+      {this.text,
+      this.domain,
+      this.by,
+      this.score,
+      this.age,
+      this.commentCount});
+}
+
+final articles = [
+  Article(
+    text: 'Circular Shock leads the way into glory',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'www.wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+
+  Article(
+    text: 'Circular Shock',
+    age: '12/12/14',
+    by: 'Ramona',
+    commentCount: 32,
+    domain: 'wiley.com',
+    score: 12,
+  ),
+];
